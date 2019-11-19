3acebf4b796bf0bd472935a4ffc5a437696917f9
Walber Beltrame <walber.beltrame@gmail.com>
2019-11-19 12:26:41 -0300

    Versão inicial da página principal

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,65 @@
 const { task, src, dest, series, parallel } = require("gulp");
-const babel = require("gulp-babel");
-const eslint = require("gulp-eslint");
+const csso = require("gulp-csso");
+const htmlmin = require("gulp-htmlmin");
 const rename = require("gulp-rename");
-const terser = require("gulp-terser");
 
 /**
  * 
- * Task of build from source and generate javascript distribution file.
+ * Task of build from library and generate public files.
  * 
  */
-task("javascript", function() {
- return src("src/molded.js")
-  .pipe(eslint())
-  .pipe(rename("molded.min.js"))
-  .pipe(terser())
-  .pipe(dest("dist/"));
+task("lib/material-design", () => {
+  return src("node_modules/material-design-icons/iconfont/material-icons.css")
+   .pipe(csso())
+   .pipe(rename("material-icons.min.css"))
+   .pipe(dest("public/2019/lib/material-design-icons/iconfont/"));
 });
-
-/**
- * 
- * Task of build from source and generate babel javascript distribution file.
- * 
- */
-task("babel", function() {
-  return src("src/molded.js")
-   .pipe(eslint())
-   .pipe(babel({
-    presets: ["@babel/env"]
-   }))
-   .pipe(rename("molded.babel.min.js"))
-   .pipe(terser())
-   .pipe(dest("dist/"));
- });
-
-/**
- * 
- * Tasks of build from source and generate dart distribution file.
- * 
- */
-task("dart/lib", function() {
- return src("src/molded.dart")
-  .pipe(dest("lib/"))
-  .pipe(dest("dart/lib/"));
+task("lib/material-design-icons", () => {
+  return src(["node_modules/material-design-icons/iconfont/MaterialIcons-Regular.eot",
+              "node_modules/material-design-icons/iconfont/MaterialIcons-Regular.ijmap",
+              "node_modules/material-design-icons/iconfont/MaterialIcons-Regular.svg",
+              "node_modules/material-design-icons/iconfont/MaterialIcons-Regular.ttf",
+              "node_modules/material-design-icons/iconfont/MaterialIcons-Regular.woff",
+              "node_modules/material-design-icons/iconfont/MaterialIcons-Regular.woff2"])
+   .pipe(dest("public/2019/lib/material-design-icons/iconfont/"));
 });
-task("dart/test", function() {
- return src("test/molded_test.dart")
-  .pipe(dest("dart/test/"));
-});
-task("dart/files", function() {
- return src(["README.md","CHANGELOG.md","LICENSE","pubspec.yaml"])
-  .pipe(dest("dart/"));
+task("lib/material-design-lite", () => {
+  return src(["node_modules/material-design-lite/dist/material.min.js",
+              "node_modules/material-design-lite/dist/material.min.js.map",
+              "node_modules/material-design-lite/dist/material.indigo-pink.min.css"])
+   .pipe(dest("public/2019/lib/material-design-lite/dist/"));
 });
 
 /**
  * 
- * Tasks of build from source and generate dotnet distribution file.
+ * Task of copy and paste images public files.
  * 
  */
-task("dotnet/lib", function() {
- return src(["src/Molded.cs","src/Molded.csproj"])
-  .pipe(dest("dotnet/Molded/"));
+task("images", () => {
+  return src("src/2019/images/*.*")
+   .pipe(dest("public/2019/images/"));
 });
-task("dotnet/test", function() {
- return src(["test/MoldedTest.cs","test/MoldedTest.csproj"])
-  .pipe(dest("dotnet/Molded.Tests/"));
- });
-task("dotnet/files", function() {
- return src(["Molded.sln"])
-  .pipe(dest("dotnet/"));
- });
 
 /**
  * 
- * Tasks of build from source and generate python distribution file.
+ * Task of build from source and generate html public files.
  * 
  */
-task("python/lib", function() {
- return src("src/molded.py")
-  .pipe(dest("lib/"))
-  .pipe(dest("python/lib/"));
+task("html", () => {
+ return src("src/2019/*.html")
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(dest("public/2019/"));
 });
-task("python/test", function() {
- return src("test/moldedtest.py")
-  .pipe(dest("python/"));
+
+/**
+ * 
+ * Task of build from source and generate index public file.
+ * 
+ */
+task("index", () => {
+ return src("src/index.html")
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(dest("public/"));
 });
 
 /**
@@ -90,4 +67,4 @@ task("python/test", function() {
  * Default task.
  * 
  */
-task("default", series("javascript", "babel", parallel("dart/lib", "dart/test", "dart/files"), parallel("dotnet/lib", "dotnet/test", "dotnet/files"), parallel("python/lib", "python/test")));
+task("default", series(parallel("lib/material-design","lib/material-design-icons","lib/material-design-lite"), "images", "html", "index"));
