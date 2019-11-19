e97acc71841ff4e2b42ac9ebba510e6a7de5fe4e
Walber Beltrame <walber.beltrame@gmail.com>
2019-11-19 11:01:01 -0300

    Primeira versão dos arquivos

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+* Primeira versão do site do Seminário de Tecnologias Assistivas do Ifes Guarapari de 2019
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+const { task, src, dest, series, parallel } = require("gulp");
+const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
+const rename = require("gulp-rename");
+const terser = require("gulp-terser");
+
+/**
+ * 
+ * Task of build from source and generate javascript distribution file.
+ * 
+ */
+task("javascript", function() {
+ return src("src/molded.js")
+  .pipe(eslint())
+  .pipe(rename("molded.min.js"))
+  .pipe(terser())
+  .pipe(dest("dist/"));
+});
+
+/**
+ * 
+ * Task of build from source and generate babel javascript distribution file.
+ * 
+ */
+task("babel", function() {
+  return src("src/molded.js")
+   .pipe(eslint())
+   .pipe(babel({
+    presets: ["@babel/env"]
+   }))
+   .pipe(rename("molded.babel.min.js"))
+   .pipe(terser())
+   .pipe(dest("dist/"));
+ });
+
+/**
+ * 
+ * Tasks of build from source and generate dart distribution file.
+ * 
+ */
+task("dart/lib", function() {
+ return src("src/molded.dart")
+  .pipe(dest("lib/"))
+  .pipe(dest("dart/lib/"));
+});
+task("dart/test", function() {
+ return src("test/molded_test.dart")
+  .pipe(dest("dart/test/"));
+});
+task("dart/files", function() {
+ return src(["README.md","CHANGELOG.md","LICENSE","pubspec.yaml"])
+  .pipe(dest("dart/"));
+});
+
+/**
+ * 
+ * Tasks of build from source and generate dotnet distribution file.
+ * 
+ */
+task("dotnet/lib", function() {
+ return src(["src/Molded.cs","src/Molded.csproj"])
+  .pipe(dest("dotnet/Molded/"));
+});
+task("dotnet/test", function() {
+ return src(["test/MoldedTest.cs","test/MoldedTest.csproj"])
+  .pipe(dest("dotnet/Molded.Tests/"));
+ });
+task("dotnet/files", function() {
+ return src(["Molded.sln"])
+  .pipe(dest("dotnet/"));
+ });
+
+/**
+ * 
+ * Tasks of build from source and generate python distribution file.
+ * 
+ */
+task("python/lib", function() {
+ return src("src/molded.py")
+  .pipe(dest("lib/"))
+  .pipe(dest("python/lib/"));
+});
+task("python/test", function() {
+ return src("test/moldedtest.py")
+  .pipe(dest("python/"));
+});
+
+/**
+ * 
+ * Default task.
+ * 
+ */
+task("default", series("javascript", "babel", parallel("dart/lib", "dart/test", "dart/files"), parallel("dotnet/lib", "dotnet/test", "dotnet/files"), parallel("python/lib", "python/test")));
